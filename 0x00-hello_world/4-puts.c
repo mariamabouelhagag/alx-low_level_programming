9594c61948a155030ce0733d7155d52f7303b443
mariam <maraiamsalama888@gmail.com>
2023-06-15 17:33:40 -0700

    first c program

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A program that print a line with input function
+ * Return : 0 (Success)
+ */
+int main (void)
+{puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
