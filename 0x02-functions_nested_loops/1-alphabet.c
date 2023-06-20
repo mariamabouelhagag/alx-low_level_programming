d9ed812b44497bccfabd39571bc84d7431d02ae1
mariam <maraiamsalama888@gmail.com>
2023-06-20 13:10:03 -0700

    lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * main -  print the alphabet, in lowercase
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char letter;
+	for(letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
