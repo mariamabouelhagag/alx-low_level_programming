dec41cb8adf0626254e839d68b3e8a0ade12c519
mariam <maraiamsalama888@gmail.com>
2023-07-06 02:33:01 -0700

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -6,15 +6,11 @@
  *
  * Return: result of the power
  */
-
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-	{
+	if (y < 0)
 		return (-1);
-	}
-	if else
-	{
-		return (x ** y);
-	}
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
 }
