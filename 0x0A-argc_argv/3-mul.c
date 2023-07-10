05a5f81dc1a10c98189bb2c47be6c0de137e58be
mariam <maraiamsalama888@gmail.com>
2023-07-10 14:10:45 -0700

    mult

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 #define UNUSED(x) (void)(x)
 /**
  * main - program that multiplies two numbers
@@ -13,7 +14,7 @@ int main(int argc, char *argv[])
 
 	if (argc == 3)
 	{
-		times = _atoi(argv[1]) * _atoi(argv[2]);
+		times = atoi(argv[1]) * atoi(argv[2]);
 		printf("%i\n", times);
 
 		return (0);
@@ -24,4 +25,5 @@ int main(int argc, char *argv[])
 
 		return (1);
 	}
+	return (0);
 }
