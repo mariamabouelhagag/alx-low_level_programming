f44edcae89f9b4c1d431d5d1ad47f55b1df81242
mariam <maraiamsalama888@gmail.com>
2023-07-10 09:22:50 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,13 +7,13 @@
  * @argv: An array containing the program command line arguments
  * Return: always 0
  */
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 	int times;
 
 	if (argc == 3)
 	{
-		times = ((argv[1]) * (argv[2]));
+		times = _atoi(argv[1]) * _atoi(argv[2]);
 		printf("%i\n", times);
 
 		return (0);
