639a7aa8e591d3c193a865a78b23534d309295fd
mariam <maraiamsalama888@gmail.com>
2023-07-10 09:11:12 -0700

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - program that multiplies two numbers
+ * @argc: the size of the argv array.
+ * @argv: An array containing the program command line arguments
+ * Return: always 0
+ */
+int main(int argc, int *argv[])
+{
+	int times;
+
+	if (argc == 3)
+	{
+		times = ((argv[1]) * (argv[2]));
+		printf("%i\n", times);
+
+		return (0);
+	}
+	else if (argc != 3)
+	{
+		printf("Error\n");
+
+		return (1);
+	}
+}
