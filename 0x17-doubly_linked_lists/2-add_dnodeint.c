8f0da4d0651f999460e5032f73940006010c2e65
mariam salama <mariamsalama888@gmail.com>
2023-10-13 11:01:24 -0700

    add to the head

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -6,24 +6,20 @@
  * Return: If the function fails - NULL.
  * Otherwise - the address of the new node.
  */
-
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
-	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new;
 
+	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
-	{
 		return (NULL);
-	}
 
 	new->n = n;
-	new->next = *head;
 	new->prev = NULL;
-
-	if (head != NULL)
-	{
+	new->next = *head;
+	if (*head != NULL)
 		(*head)->prev = new;
-	}
 	*head = new;
+
 	return (new);
 }
