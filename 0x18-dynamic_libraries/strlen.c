c589c24f15e3138c5405e2e14955c5696a24f0e1
mariam salama <mariamsalama888@gmail.com>
2023-10-16 13:53:04 -0700

    0x18

---
+++ 0x18-dynamic_libraries/strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - function that returns the length of a string
+ * @s: Pointer to the null-terminated string
+ *
+ * Return: the length of the string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
