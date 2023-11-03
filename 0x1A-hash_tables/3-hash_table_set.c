423e575e592ed97acbd473020f72d68405491324
mariam salama <mariamsalama888@gmail.com>
2023-11-03 14:45:33 -0700

    node

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - Add or update an element in a hash table.
+ * @ht: A pointer to the hash table.
+ * @key: The key to add - cannot be an empty string.
+ * @value: The value associated with key.
+ *
+ * Return: Upon failure - 0.
+ *         Otherwise - 1.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index = key_index(key, ht->size);
+	hash_node_t *new_node = malloc(sizeof(hash_node_t));
+
+	if (new_node == NULL)
+		return (0);
+
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
+}
