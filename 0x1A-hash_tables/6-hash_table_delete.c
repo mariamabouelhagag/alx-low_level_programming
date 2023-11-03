235e835d567fe11c382445992759f5f821c7a040
mariam salama <mariamsalama888@gmail.com>
2023-11-03 16:44:09 -0700

    delet

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,29 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - Deletes a hash table.
+ * @ht: A pointer to a hash table.
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		hash_node_t *node = ht->array[i];
+
+		while (node != NULL)
+		{
+			hash_node_t *tmp = node;
+
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
---
+++ 0x1A-hash_tables/test_files/6-hash_table_delete.c
@@ -0,0 +1,26 @@
+#include "hash_tables.h"
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+
+	if (ht == NULL)
+		return;
+
+	for (i = 0; i < ht->size; i++)
+	{
+		hash_node_t *node = ht->array[i];
+
+		while (node != NULL)
+		{
+			hash_node_t *tmp = node;
+
+			node = node->next;
+			free(tmp->key);
+			free(tmp->value);
+			free(tmp);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
